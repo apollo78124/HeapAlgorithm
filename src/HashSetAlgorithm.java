9c33ecea2ca43413dff25169031cc6c20fbfe881
apollo78124 <98apollo@naver.com>
2020-06-17 19:09:47 -0700

    Check word uniqueness using hashmap

--- src/HashSetAlgorithm.java
+++ src/HashSetAlgorithm.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class HashSetAlgorithm {
 
@@ -18,12 +19,45 @@ public class HashSetAlgorithm {
 		}
 		Iterator<String> iterator = hashSet1.iterator(); 
 		int j = 1;
+		System.out.println("Question 1: ");
         while (iterator.hasNext())  {
             System.out.println("String" + j + " : " + iterator.next()); 
             j++;
         }
         
         //Question 2 
+        System.out.println("Question 2 :");
+        Scanner inFile2 = new Scanner(new File("q1input.txt"));
+        TreeSet<String> treeSet1 = new TreeSet<String>();
+
+		while (inFile2.hasNextLine()) {
+			treeSet1.add(inFile2.nextLine());
+		}
+		Iterator<String> iterator2 = treeSet1.iterator(); 
+		j = 1;
+        while (iterator2.hasNext())  {
+            System.out.println("String" + j + " : " + iterator2.next()); 
+            j++;
+        }
+        
+        //Question 3
+        Scanner inFile3 = new Scanner(new File("q1input.txt"));
+		HashSet<String> hashSet2 = new HashSet<String>(10);
+
+		boolean distinct = true;
+		while (inFile3.hasNextLine()) {
+			String word = inFile3.nextLine();
+			if (hashSet2.contains(word))
+				distinct = false;
+			hashSet2.add(word);
+		}
+		System.out.println("Question 3: ");
+        if (distinct) {
+        	System.out.println("DISTINCT");
+        } else {
+        	System.out.println("NOT DISTINCT");
+        }
+        
         
         
     } 
